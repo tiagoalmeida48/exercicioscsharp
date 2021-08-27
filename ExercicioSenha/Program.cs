2ccd52f44dcdcd6b6cb2a1a91f0fde026adb85a4
Tiago Almeida <53452661+tiagoalmeida48@users.noreply.github.com>
2021-08-27 19:19:39 -0300

    Add files via upload

---
+++ ExercicioSenha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123456";
+            string validaSenha;
+
+            Console.Write("Digite sua senha: ");
+            validaSenha = Console.ReadLine();
+
+            while (senha != validaSenha)
+            {
+                Console.WriteLine("Senha invalida!\n");
+                Console.Write("Digite sua senha: ");
+                validaSenha = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
