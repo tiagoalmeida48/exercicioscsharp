6587763ebfb1db7ee572dcfd0b745950115d0ebf
Tiago Almeida <53452661+tiagoalmeida48@users.noreply.github.com>
2021-08-27 19:23:04 -0300

    Add files via upload

---
+++ ExercicioRaio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.141459;
+            double raio, area = 0.0;
+
+            Console.Write("Entre com o raio do circulo : ");
+            raio = double.Parse(Console.ReadLine());
+
+            area = PI * (raio * raio);
+
+            Console.Write("Área do Círculo : " + area);
+        }
+    }
+}
